61f547f4996472ce8aa4283a53622fec67f22699
Dan Newcome <dnewcome@elevenprime.com>
2011-09-17 20:30:54 -0700

    conversion from record set to js objects pretty close to working

--- server.js
+++ server.js
@@ -9,18 +9,107 @@ app.use(express.static( __dirname + '/static' ));
 
 // TODO: extend search to do ordering and limit
 app.post('/query', function(req, res){
+	var joins = [];
 	console.log( "querying: " + req.body );
 	var client = getClient();
+	var sql = "select " + cslist( getfields( req.body[0], req.body[1], joins ) ) + " from " + req.body[0] + joinClause( joins ); 
+	console.log( sql );
 	client.query(
-		"select " + cslist( req.body.fields ) + " from " + req.body.table, 
-		[], 
+		sql, [], 
 		function( err, results ) { 
-			res.send( results );
+			// res.send( results );
+			res.send( disjoin_set( req.body, results ) );
 			client.end();
 	});
 });
 
+/**
+ * Convert array to comma separated list
+ */
 function cslist( arr ) {
 	return arr.join(',');
 }
+
+/**
+ * Convert array to comma separated list
+ */
+function joinClause( arr ) {
+	var ret = " ";
+	for( var i=0; i < arr.length; i++ ) {
+		// TODO: join clause is hard coded
+		ret += "join " + arr[i] + " on fk_parentid = parent.id "
+	}
+	return ret;
+}
+
+/**
+ * Return comma separated list of fields
+ * from object spec
+ */
+function getfields( table, obj, joins ) {
+	var arr = [];
+	for( item in obj ) {
+		console.log( 'fields: ' + item + ' ' + obj[item] );
+		if( typeOf( obj[item] ) == 'array' ) {
+			joins.push( obj[item][0] );
+			arr = arr.concat( getfields( obj[item][0], obj[item][1], joins ) );
+		}
+		else {
+			arr.push( table + '.' + obj[item] + ' as ' + table + '_' + obj[item]  );	
+		}
+	}
+	return arr;
+}
+
+/**
+ * Reconstruct json object from record set
+ * NOte: does not properly normalize data yet.
+ * will need to merge records with same parent together
+ */
+function disjoin_set( query, recordset, idkey, idval ) {
+	console.log( idkey + ' ' + idval )
+	var ret = [];
+	for( var i=0; i < recordset.length; i++ ) {
+		if( typeof idkey != 'undefined' ) {
+			if( recordset[idkey] == idval ) {
+				ret.push( disjoin( query, recordset[i], recordset ) );
+			}
+		}
+		else {
+			ret.push( disjoin( query, recordset[i], recordset ) );
+		}
+	}
+	return ret;
+}
+function disjoin( query, record, recordset ) {
+	var ret = {};
+	var table = query[0];
+	for( item in query[1] ) {
+		if( typeOf( query[1][item] ) == 'array' ) { 
+			ret[item] = disjoin_set( query[1][item], recordset, item, query[1][item] );
+		}
+		else {
+			ret[item] = record[table+'_'+item];
+		}
+	}
+	return ret;
+}
+
+
+// crockford's typeOf
+function typeOf(value) {
+    var s = typeof value;
+    if (s === 'object') {
+        if (value) {
+            if (typeof value.length === 'number' &&
+                    !(value.propertyIsEnumerable('length')) &&
+                    typeof value.splice === 'function') {
+                s = 'array';
+            }
+        } else {
+            s = 'null';
+        }
+    }
+    return s;
+} 
 app.listen(3000);
