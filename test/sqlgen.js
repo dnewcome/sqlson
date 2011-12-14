fa046358e48eaddc351aaf3f774d851661ed07b1
Dan Newcome <dnewcome@elevenprime.com>
2011-12-13 18:22:48 -0800

    fixes to to-json so that we support join criteria now

--- test/sqlgen.js
+++ test/sqlgen.js
@@ -5,6 +5,7 @@ exports.testSqlGeneration = function() {
 	// in this example, join criteria are hard coded in the system.
 	// also there are no filters.
 	var query = [ 
+		// note that 'where' relation is optional, but join relation is not
 		"parent", { 
 			"id":"id", 
 			"text":"text",
@@ -47,25 +48,56 @@ exports.testSqlGenerationWhere = function() {
 	assert.equal(expected, actual);
 }
 
-/*
-exports.testSqlGenerationWhereJoin = function() {
-	// in this example, join criteria are explicitly specified.
-	// Filter is on id.
+exports.testSqlGenerationMapping = function() {
+	// change field names
+	// shouldn't change generated sql, but affects how things are deserialized.
 	var query = [ 
-		"parent", "parent.id = 1", { 
-			"id":"id", 
-			"text":"text",
+		"parent", { 
+			"idd":"id", 
+			"textt":"text",
 			"children": [ 
 				"child", "child.fk_parentid = parent.id", { 
-					"id":"id", 
-					"text":"text"
+					"iddd":"id", 
+					"texttt":"text"
 				} 
 			]
 		} 
 	];
 
-	var actual = norm.genSql( query[0], query[2], query[1] );
+	var actual = norm.genSql( query[0], query[1] );
 	// note the trailing space
-	var expected = "select parent.id as parent_id,parent.text as parent_text,child.id as child_id,child.text as child_text from parent join child on fk_parentid = parent.id where parent.id = 1";
+	var expected = "select parent.id as parent_id,parent.text as parent_text,child.id as child_id,child.text as child_text from parent join child on child.fk_parentid = parent.id ";
+
+	assert.equal(expected, actual);
 }
-*/
+
+exports.testJsonGen = function() {
+	var getClient = require('../getclient.js').getClient;
+	var disjoin = require('../to_json-join').disjoin_set;
+
+	var query = [ 
+		"parent", { 
+			"idd":"id", 
+			"textt":"text",
+			"children": [ 
+				"child", "child.fk_parentid = parent.id", { 
+					"iddd":"id", 
+					"texttt":"text"
+				} 
+			]
+		} 
+	];
+	var client = getClient();
+	var sql = norm.genSql( query[0], query[1] ); 
+	console.log( "testJsonGen: "  + sql );
+	console.log( sql );
+	client.query(
+		sql, [], 
+		function( err, results ) { 
+			console.log( results );
+			// console.log( disjoin( query, results ) );
+			console.log( JSON.stringify( disjoin( query, results ), null, 4 ) );
+			client.end();
+		}
+	);
+};
--- to_json-join.js
+++ to_json-join.js
@@ -1,15 +1,22 @@
 /**
  * Reconstruct json object array from record set
+ * Iterate over composite recordset one row at a time
+ *
+ * query - json query spec
+ * recorset - tabular db result
+ * idkey - if this is a child, what is the join key
+ * idval - if child, join value
  */
 function disjoin_set( query, recordset, idkey, idval ) {
-	console.log( idkey + ' ' + idval )
 	var ret = [];
 	for( var i=0; i < recordset.length; i++ ) {
+		// nested objects, check for parent.
 		if( typeof idkey != 'undefined' ) {
 			if( recordset[i][idkey] == idval ) {
 				ret.push( disjoin( query, recordset[i], recordset ) );
 			}
 		}
+		// toplevel objects - no parent
 		else {
 			ret.push( disjoin( query, recordset[i], recordset ) );
 		}
@@ -23,12 +30,22 @@ function disjoin_set( query, recordset, idkey, idval ) {
 function disjoin( query, record, recordset ) {
 	var ret = {};
 	var table = query[0];
-	for( item in query[1] ) {
-		if( typeOf( query[1][item] ) == 'array' ) { 
-			ret[item] = disjoin_set( query[1][item], recordset, table+'_id', record[table+'_id'] );
+	// case that there is a 'where' clause
+	// This is kind of a hack. Probably should try to simplify this
+	if( typeOf( query[1] ) == 'string' ) {
+		var subquery = query[2];
+	}
+	else {
+		var subquery = query[1];
+	}
+	for( item in subquery ) {
+		if( typeOf( subquery[item] ) == 'array' ) { 
+			var val = subquery[item];
+			ret[item] = disjoin_set( val, recordset, table+'_id', record[table+'_id'] );
 		}
 		else {
-			ret[item] = record[table+'_'+item];
+			var val = subquery[item]
+			ret[item] = record[table+'_'+val];
 		}
 	}
 	return ret;
